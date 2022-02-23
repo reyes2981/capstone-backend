ce22aff780788f83686fc151569c8ccaf6d0d0f7
noe <noe.reyes92@yahoo.com>
2022-02-23 11:16:12 -0600

    added dependencies to POM file

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,30 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.management.relation.Role;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private String username;
+
+
 }
