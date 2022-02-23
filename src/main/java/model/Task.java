e935d772b9ee93bc0f397836f4b1aacc10e29748
noe <noe.reyes92@yahoo.com>
2022-02-23 11:27:49 -0600

    added constructor, getters, setters to task class

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -1,8 +1,38 @@
 package model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 public class Task implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // research this line more
+    @Column(nullable = false, updatable = false) // research this line more
     private Long id;
     private String body;
+
+    public Task() {
+    }
+
+    public Task(Long id, String body) {
+        this.id = id;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
