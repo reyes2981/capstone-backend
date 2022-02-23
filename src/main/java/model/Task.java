9df019b2076697783b9faf1cd54cee61f18945a0
noe <noe.reyes92@yahoo.com>
2022-02-23 11:23:20 -0600

    created task model and implemented serializable interface for better communication w/ frontend

---
+++ src/main/java/model/Task.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private Long id;
+    private String body;
+}
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.management.relation.Role;
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Collection;
 
 @Entity
 @Data
