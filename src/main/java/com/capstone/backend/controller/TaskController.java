b806879054a7c95e0c014917aae4f528e6f9f02d
noe <noe.reyes92@yahoo.com>
2022-02-24 14:31:47 -0600

    added CRUD functionlity logic to task controller

--- src/main/java/com/capstone/backend/controller/TaskController.java
+++ src/main/java/com/capstone/backend/controller/TaskController.java
@@ -1,22 +1,77 @@
 package com.capstone.backend.controller;
 
+import com.capstone.backend.model.Response;
 import com.capstone.backend.model.Task;
 import com.capstone.backend.service.TaskService;
 import com.capstone.backend.service.TaskServiceImplementation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
 
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("/task")
 @RequiredArgsConstructor
 public class TaskController {
 
+    private final TaskServiceImplementation taskService;
 
+    @GetMapping("/list")
+    public ResponseEntity<Response> getTasks() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("tasks", taskService.list(30)))
+                        .message("Tasks retreived")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
 
+    @PostMapping("/save")
+    public ResponseEntity<Response> saveTask(@RequestBody @Validated Task task) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("task", taskService.create(task)))
+                        .message("Task created")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Response> getTask(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("task", taskService.get(id)))
+                        .message("Task retreived")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Response> DeleteTask(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("deleted", taskService.delete(id)))
+                        .message("Task deleted")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        );
+    }
 
 }
--- src/main/java/com/capstone/backend/model/Response.java
+++ src/main/java/com/capstone/backend/model/Response.java
@@ -1,11 +1,17 @@
 package com.capstone.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
 //class represents the response that is sent to user everytime they make a request to API
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
 
     protected LocalDateTime timeStamp;
