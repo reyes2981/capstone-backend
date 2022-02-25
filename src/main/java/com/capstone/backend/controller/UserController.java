fddcdffe09cfb7840ac5fc58c88190ec915efd06
noe <noe.reyes92@yahoo.com>
2022-02-25 14:40:43 -0600

    added a getallusers and getuserbyid method inside of user controller

--- src/main/java/com/capstone/backend/controller/UserController.java
+++ src/main/java/com/capstone/backend/controller/UserController.java
@@ -1,8 +1,15 @@
 package com.capstone.backend.controller;
 
+import com.capstone.backend.model.User;
 import com.capstone.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -12,4 +19,21 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.findAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        User user = userService.findUserById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
 }
+
+//ResponseEntity represents an HTTP response, including headers,
+// body, and status. While @ResponseBody puts the return value into
+// the body of the response, ResponseEntity also allows us to add headers
+// and status code.
