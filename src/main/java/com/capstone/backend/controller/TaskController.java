926387f6adae06954328b14d9ecee2a733e43de4
noe <noe.reyes92@yahoo.com>
2022-02-27 15:59:27 -0600

    refactored all methods in task controller to similar boilerplate code from user controller

--- src/main/java/com/capstone/backend/controller/TaskController.java
+++ src/main/java/com/capstone/backend/controller/TaskController.java
@@ -2,12 +2,15 @@ package com.capstone.backend.controller;
 
 import com.capstone.backend.model.Response;
 import com.capstone.backend.model.Task;
+import com.capstone.backend.model.User;
+import com.capstone.backend.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 import static java.time.LocalDateTime.now;
@@ -18,58 +21,36 @@ import static java.time.LocalDateTime.now;
 @RequiredArgsConstructor
 public class TaskController {
 
-    private final TaskServiceImplementation taskService;
+    private final TaskService taskService;
 
     @GetMapping("/list")
-    public ResponseEntity<Response> getTasks() {
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(now())
-                        .data(Map.of("tasks", taskService.list(30)))
-                        .message("Tasks retreived")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build()
-        );
+    public ResponseEntity<List<Task>> getAllTasks() {
+        List<Task> tasks = taskService.findAllTasks();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable("id") Long id) {
+        Task task = taskService.findTaskById(id);
+        return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Response> saveTask(@RequestBody @Validated Task task) {
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(now())
-                        .data(Map.of("task", taskService.create(task)))
-                        .message("Task created")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build()
-        );
+    public ResponseEntity<User> addTask(@RequestBody Task task) {
+        Task newTask = taskService.addTask(task);
+        return new ResponseEntity<>(newTask, HttpStatus.CREATED);
     }
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<Response> getTask(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(now())
-                        .data(Map.of("task", taskService.get(id)))
-                        .message("Task retreived")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build()
-        );
+    @PutMapping("/update")
+    public ResponseEntity<Task> updateTask(@RequestBody Task task) {
+        Task updateTask = taskService.updateTask(task);
+        return new ResponseEntity<>(updateTask, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Response> DeleteTask(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(
-                Response.builder()
-                        .timeStamp(now())
-                        .data(Map.of("deleted", taskService.delete(id)))
-                        .message("Task deleted")
-                        .status(HttpStatus.OK)
-                        .statusCode(HttpStatus.OK.value())
-                        .build()
-        );
+    public ResponseEntity<?> deleteTask(@PathVariable("id") Long id) {
+        taskService.deleteTask(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- src/main/java/com/capstone/backend/controller/UserController.java
+++ src/main/java/com/capstone/backend/controller/UserController.java
@@ -2,6 +2,7 @@ package com.capstone.backend.controller;
 
 import com.capstone.backend.model.User;
 import com.capstone.backend.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +12,9 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/list")
     public ResponseEntity<List<User>> getAllUsers() {
