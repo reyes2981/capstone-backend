eaf2a54e6abe73feb23894a617b327a19a2d4ea1
noe <noe.reyes92@yahoo.com>
2022-03-01 14:41:21 -0600

    adding methods to jwtutl

--- src/main/java/com/capstone/backend/JwtUtil/JwtUtil.java
+++ src/main/java/com/capstone/backend/JwtUtil/JwtUtil.java
@@ -1,7 +1,41 @@
 package com.capstone.backend.JwtUtil;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtUtil {
+
+    private String secret = "sprite123";
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJwt(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
 }
