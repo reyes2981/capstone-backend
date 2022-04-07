897b28e12c75f6a4778bb8a2153c1e2cd9145bc7
noe <noe.reyes92@yahoo.com>
2022-04-07 17:45:43 -0500

    created a registration request and service

--- src/main/java/com/capstone/backend/controller/RegistrationController.java
+++ src/main/java/com/capstone/backend/controller/RegistrationController.java
@@ -1,4 +1,20 @@
 package com.capstone.backend.controller;
 
+import com.capstone.backend.registration.RegistrationRequest;
+import com.capstone.backend.service.RegistrationService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
 public class RegistrationController {
+
+    private RegistrationService registrationService;
+
+    public String register(@RequestBody RegistrationRequest request) {
+        return registrationService.register(request);
+    }
 }
---
+++ src/main/java/com/capstone/backend/service/RegistrationService.java
@@ -0,0 +1,9 @@
+package com.capstone.backend.service;
+
+import com.capstone.backend.registration.RegistrationRequest;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
