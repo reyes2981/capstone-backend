2bd87bd03d60f6943835d152788b731125e3d98f
noe <noe.reyes92@yahoo.com>
2022-04-11 18:14:01 -0500

    added logic to doaauthenticationprovider method

--- src/main/java/com/capstone/backend/service/RegistrationService.java
+++ src/main/java/com/capstone/backend/service/RegistrationService.java
@@ -1,7 +1,9 @@
 package com.capstone.backend.service;
 
 import com.capstone.backend.registration.RegistrationRequest;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "works";
